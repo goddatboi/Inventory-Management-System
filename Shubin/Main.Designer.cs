984e8651e3c23d4a9e5aa430af59a999d17ad31c
dat boi <kreepsyzer@gmail.com>
2023-01-11 20:31:16 +0300

    Вывод данных из БД в DataGridView

--- Shubin/Main.Designer.cs
+++ Shubin/Main.Designer.cs
@@ -46,13 +46,15 @@
             this.textBox_PurchaseDate = new System.Windows.Forms.TextBox();
             this.textBox_Status = new System.Windows.Forms.TextBox();
             this.ID = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // label7
             // 
             this.label7.AutoSize = true;
             this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label7.Location = new System.Drawing.Point(167, 544);
+            this.label7.Location = new System.Drawing.Point(71, 547);
             this.label7.Name = "label7";
             this.label7.Size = new System.Drawing.Size(100, 22);
             this.label7.TabIndex = 35;
@@ -62,7 +64,7 @@
             // 
             this.label6.AutoSize = true;
             this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label6.Location = new System.Drawing.Point(141, 517);
+            this.label6.Location = new System.Drawing.Point(45, 520);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(124, 22);
             this.label6.TabIndex = 34;
@@ -72,7 +74,7 @@
             // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label5.Location = new System.Drawing.Point(112, 488);
+            this.label5.Location = new System.Drawing.Point(16, 491);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(155, 22);
             this.label5.TabIndex = 33;
@@ -82,7 +84,7 @@
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label4.Location = new System.Drawing.Point(112, 456);
+            this.label4.Location = new System.Drawing.Point(16, 459);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(154, 22);
             this.label4.TabIndex = 32;
@@ -92,7 +94,7 @@
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label3.Location = new System.Drawing.Point(191, 426);
+            this.label3.Location = new System.Drawing.Point(95, 429);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(74, 22);
             this.label3.TabIndex = 31;
@@ -102,7 +104,7 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label2.Location = new System.Drawing.Point(129, 396);
+            this.label2.Location = new System.Drawing.Point(33, 399);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(136, 22);
             this.label2.TabIndex = 30;
@@ -111,7 +113,7 @@
             // textBox_ID
             // 
             this.textBox_ID.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.textBox_ID.Location = new System.Drawing.Point(271, 366);
+            this.textBox_ID.Location = new System.Drawing.Point(175, 369);
             this.textBox_ID.Multiline = true;
             this.textBox_ID.Name = "textBox_ID";
             this.textBox_ID.Size = new System.Drawing.Size(229, 24);
@@ -119,7 +121,7 @@
             // 
             // saveButton
             // 
-            this.saveButton.Location = new System.Drawing.Point(701, 520);
+            this.saveButton.Location = new System.Drawing.Point(650, 523);
             this.saveButton.Name = "saveButton";
             this.saveButton.Size = new System.Drawing.Size(117, 46);
             this.saveButton.TabIndex = 21;
@@ -128,7 +130,7 @@
             // 
             // deleteButton
             // 
-            this.deleteButton.Location = new System.Drawing.Point(701, 468);
+            this.deleteButton.Location = new System.Drawing.Point(650, 471);
             this.deleteButton.Name = "deleteButton";
             this.deleteButton.Size = new System.Drawing.Size(117, 46);
             this.deleteButton.TabIndex = 20;
@@ -137,7 +139,7 @@
             // 
             // changeButton
             // 
-            this.changeButton.Location = new System.Drawing.Point(701, 416);
+            this.changeButton.Location = new System.Drawing.Point(650, 419);
             this.changeButton.Name = "changeButton";
             this.changeButton.Size = new System.Drawing.Size(117, 46);
             this.changeButton.TabIndex = 19;
@@ -146,7 +148,7 @@
             // 
             // createButton
             // 
-            this.createButton.Location = new System.Drawing.Point(701, 364);
+            this.createButton.Location = new System.Drawing.Point(650, 367);
             this.createButton.Name = "createButton";
             this.createButton.Size = new System.Drawing.Size(117, 46);
             this.createButton.TabIndex = 18;
@@ -156,7 +158,7 @@
             // textBox_Name
             // 
             this.textBox_Name.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.textBox_Name.Location = new System.Drawing.Point(271, 396);
+            this.textBox_Name.Location = new System.Drawing.Point(175, 399);
             this.textBox_Name.Multiline = true;
             this.textBox_Name.Name = "textBox_Name";
             this.textBox_Name.Size = new System.Drawing.Size(229, 24);
@@ -165,7 +167,7 @@
             // textBox_Model
             // 
             this.textBox_Model.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.textBox_Model.Location = new System.Drawing.Point(271, 426);
+            this.textBox_Model.Location = new System.Drawing.Point(175, 429);
             this.textBox_Model.Multiline = true;
             this.textBox_Model.Name = "textBox_Model";
             this.textBox_Model.Size = new System.Drawing.Size(229, 24);
@@ -174,7 +176,7 @@
             // textBox_SerialNumber
             // 
             this.textBox_SerialNumber.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.textBox_SerialNumber.Location = new System.Drawing.Point(271, 456);
+            this.textBox_SerialNumber.Location = new System.Drawing.Point(175, 459);
             this.textBox_SerialNumber.Multiline = true;
             this.textBox_SerialNumber.Name = "textBox_SerialNumber";
             this.textBox_SerialNumber.Size = new System.Drawing.Size(229, 24);
@@ -183,7 +185,7 @@
             // textBox_Location
             // 
             this.textBox_Location.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.textBox_Location.Location = new System.Drawing.Point(271, 486);
+            this.textBox_Location.Location = new System.Drawing.Point(175, 489);
             this.textBox_Location.Multiline = true;
             this.textBox_Location.Name = "textBox_Location";
             this.textBox_Location.Size = new System.Drawing.Size(229, 24);
@@ -192,7 +194,7 @@
             // textBox_PurchaseDate
             // 
             this.textBox_PurchaseDate.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.textBox_PurchaseDate.Location = new System.Drawing.Point(271, 515);
+            this.textBox_PurchaseDate.Location = new System.Drawing.Point(175, 518);
             this.textBox_PurchaseDate.Multiline = true;
             this.textBox_PurchaseDate.Name = "textBox_PurchaseDate";
             this.textBox_PurchaseDate.Size = new System.Drawing.Size(229, 24);
@@ -201,7 +203,7 @@
             // textBox_Status
             // 
             this.textBox_Status.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.textBox_Status.Location = new System.Drawing.Point(271, 545);
+            this.textBox_Status.Location = new System.Drawing.Point(175, 548);
             this.textBox_Status.Multiline = true;
             this.textBox_Status.Name = "textBox_Status";
             this.textBox_Status.Size = new System.Drawing.Size(229, 24);
@@ -211,17 +213,29 @@
             // 
             this.ID.AutoSize = true;
             this.ID.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.ID.Location = new System.Drawing.Point(239, 366);
+            this.ID.Location = new System.Drawing.Point(143, 369);
             this.ID.Name = "ID";
             this.ID.Size = new System.Drawing.Size(27, 22);
             this.ID.TabIndex = 42;
             this.ID.Text = "ID";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(36, 71);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.Size = new System.Drawing.Size(885, 266);
+            this.dataGridView1.TabIndex = 43;
             // 
             // Main
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(961, 587);
+            this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.ID);
             this.Controls.Add(this.textBox_Status);
             this.Controls.Add(this.textBox_PurchaseDate);
@@ -242,6 +256,8 @@
             this.Controls.Add(this.createButton);
             this.Name = "Main";
             this.Text = "Main";
+            this.Load += new System.EventHandler(this.Main_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -267,5 +283,6 @@
         private System.Windows.Forms.TextBox textBox_PurchaseDate;
         private System.Windows.Forms.TextBox textBox_Status;
         private System.Windows.Forms.Label ID;
+        private System.Windows.Forms.DataGridView dataGridView1;
     }
 }
--- Shubin/Main.cs
+++ Shubin/Main.cs
@@ -7,14 +7,67 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace Shubin
 {
+    enum RowState
+    {
+        Existed,
+        New,
+        Modified,
+        ModifiedNew,
+        Deleted
+    }
+
     public partial class Main : Form
     {
+        DataBaseConnection dataBase = new DataBaseConnection();
         public Main()
         {
             InitializeComponent();
+        }
+
+        private void CreateColumns()
+        {
+            dataGridView1.Columns.Add("ID", "ID");
+            dataGridView1.Columns.Add("Name", "Наименование");
+            dataGridView1.Columns.Add("Model", "Модель");
+            dataGridView1.Columns.Add("SerialNumber", "Серийный номер");
+            dataGridView1.Columns.Add("Location", "Местоположение");
+            dataGridView1.Columns.Add("PurchaseDate", "Дата покупки");
+            dataGridView1.Columns.Add("Status", "Состояние");
+            dataGridView1.Columns.Add("IsNew", string.Empty);
+        }
+
+        private void ReadSingleRow(DataGridView DGW, IDataRecord record)
+        {
+            DGW.Rows.Add(record.GetInt32(0), record.GetString(1), record.GetString(2), record.GetString(3), record.GetString(4), record.GetDateTime(5), record.GetString(6), RowState.ModifiedNew);
+        }
+
+        private void RefreshDataGridView(DataGridView DGW)
+        {
+            DGW.Rows.Clear();
+            string querystring = ($"select * from InventoryItems");
+
+            SqlCommand command = new SqlCommand(querystring, dataBase.getConnection());
+
+            dataBase.openConnection();
+
+            SqlDataReader reader= command.ExecuteReader();
+
+            while (reader.Read()) 
+            {
+                ReadSingleRow(DGW, reader);
+            }
+            reader.Close();
+
+        }
+
+        private void Main_Load(object sender, EventArgs e)
+        {
+            CreateColumns();
+            RefreshDataGridView(dataGridView1);
         }
     }
 }
